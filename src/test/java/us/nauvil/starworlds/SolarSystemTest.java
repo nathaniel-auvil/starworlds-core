ce970b69d971e0df94e06cfe733f30a6f9761ccf
Nathaniel Auvil <nathaniel.auvil@gmail.com>
2016-12-10 07:36:55 -0500

    adding planets

---
+++ src/test/java/us/nauvil/starworlds/SolarSystemTest.java
@@ -0,0 +1,14 @@
+package us.nauvil.starworlds;
+
+
+import org.junit.Test;
+
+public class SolarSystemTest {
+
+    @Test
+    public void testCreate() {
+        SolarSystem s = SolarSystem.createRandom("System-1");
+        System.out.println(s);
+    }
+
+}

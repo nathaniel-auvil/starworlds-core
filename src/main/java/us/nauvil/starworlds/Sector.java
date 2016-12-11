039f20a88cc839e2babfd4465f4bef46b4a9e8ac
Nathaniel Auvil <nathaniel.auvil@gmail.com>
2016-12-11 19:27:25 +0000

    rwfw

---
+++ src/main/java/us/nauvil/starworlds/Sector.java
@@ -0,0 +1,15 @@
+package us.nauvil.starworlds;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sector {
+    
+    private List<SolarSystem> systems;
+    
+    
+    public Sector() {
+        this.systems= new ArrayList<SolarSystem>();
+    }
+    
+}

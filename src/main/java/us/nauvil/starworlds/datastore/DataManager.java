97a5306856973c58fe1f2167ad1354c8bfa1b48f
Nathaniel Auvil <nathaniel.auvil@gmail.com>
2023-10-17 01:32:47 +0000

    thinking about how to do this

---
+++ src/main/java/us/nauvil/starworlds/datastore/DataManager.java
@@ -0,0 +1,17 @@
+package us.nauvil.starworlds.datastore;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DataManager {
+    Logger logger = LoggerFactory.getLogger( DataManager.class );
+    //logger.debug("Hello world.");
+
+
+
+    public void getCurrentTick() {
+
+
+        
+    }
+}

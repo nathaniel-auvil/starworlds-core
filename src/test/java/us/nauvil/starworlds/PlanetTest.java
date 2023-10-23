011604dc43e07ccdd603a83c56fd0935fb91f2c6
Nathaniel Auvil <nathaniel.auvil@gmail.com>
2023-10-23 18:35:38 +0000

    logging

--- src/test/java/us/nauvil/starworlds/PlanetTest.java
+++ src/test/java/us/nauvil/starworlds/PlanetTest.java
@@ -2,13 +2,22 @@ package us.nauvil.starworlds;
 
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class PlanetTest {
+
+    Logger logger = LoggerFactory.getLogger( PlanetTest.class );
+
 
     @Test
     public void testCreate() {
         Planet p = Planet.createRandom(1L);
-        System.out.println(p);
+       // System.out.println(p);
+
+       
+        logger.debug(p.toString());
     }
 
 }
